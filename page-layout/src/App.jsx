d55a85d68d3e3f54914df7861e66fa621fd795f8
shams220 <103259015+shams220@users.noreply.github.com>
2025-11-22 09:00:29 +0530

    new commit

--- page-layout/src/App.jsx
+++ page-layout/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <>
-     
+      <header><h1>Welcom to my website</h1></header>
+      <p>This is the content of my website</p>
+      <footer><p>2023 My Website. All rights reserved.</p></footer>
     </>
   )
 }
